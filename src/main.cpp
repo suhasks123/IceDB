3446420dbee80bca044547fc7390acc0a9a68fa1
Sai Krishna Anand <saikrishna001207@gmail.com>
2020-06-06 20:11:24 +0530

    Comments

--- src/main.cpp
+++ src/main.cpp
@@ -12,22 +12,20 @@ int main(int argc, char** argv){
 
 // For testing:
 
-    hashClass hashObj;
-    hashObj.SetValue("Sai", "Gym");
-    hashObj.SetValue("Pachi", "Chips");
-    hashObj.SetValue("Saji", "Mallu");
-    hashObj.SetValue("Krithik", "Goan");
-    hashObj.SetValue("Hema", "Swami");
-    hashObj.SetValue("Dhanwin", "Stalker");
-    hashObj.SetValue("Arnav", "Marketer");
-    hashObj.SetValue("Sanjana", "Hairy");
-    hashObj.SetValue("Anand", "LMAO");
-    hashObj.SetValue("Suhas", "OS God");
+    // hashClass hashObj;
+    // hashObj.SetValue("Sai", "Gym");
+    // hashObj.SetValue("Pachi", "Chips");
+    // hashObj.SetValue("Saji", "Mallu");
+    // hashObj.SetValue("Krithik", "Goan");
+    // hashObj.SetValue("Dhanwin", "CP God");
+    // hashObj.SetValue("Arnav", "Marketer");
+    // hashObj.SetValue("Anand", "LMAO");
+    // hashObj.SetValue("Suhas", "OS God");
     
     // cout << hashObj.NumberOfItems(5) << endl;
 
-    hashObj.PrintTable();
-    hashObj.UpdateValue("Sai", "Mast");
+    // hashObj.PrintTable();
+    // hashObj.UpdateValue("Sai", "Mast");
     // hashObj.DeleteValue("Saji");
     // cout << "\n\n\n";
     // hashObj.PrintTable();
