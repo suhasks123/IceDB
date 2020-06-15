5b2ffff84c3fb03313c29a15be7fb50624e804af
Suhas K S <suhasks123@gmail.com>
2020-06-15 14:29:38 +0530

    Add build rules for ice shell

---
+++ src/ice_shell.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include "ice.hpp"
+
+int main()
+{
+    while(true)
+    {
+        // The instruction read
+
+        // The instruction parse - Tokenisation
+
+        // The instruction processing - Call the library
+    }
+    return 0;
+}
